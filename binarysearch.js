1ddd1f9719875d833be4655e6fe2b036e264715c
Auoja <antonauoja@gmail.com>
2014-10-15 23:15:29 +0200

    Renaming some stuff

--- binarysearch.js
+++ binarysearch.js
@@ -1,118 +1,118 @@
 (function(exports) {
 
-    function BinarySearchTree(value) {
+    function BinarySearchTree(customKeyFn) {
         var _root = null;
-        var getValue = value || function(a) {
-            return a;
+        var getKey = customKeyFn || function(key) {
+            return key;
         };
 
         // Tree Node
-        function Node(content, left, right) {
-            this.content = content;
+        function Node(value, left, right) {
+            this.value = value;
             this.leftChild = left;
             this.rightChild = right;
         }
 
-        Node.prototype.getValue = function() {
-            return getValue.call(this, this.content);
+        Node.prototype.getKey = function() {
+            return getKey.call(this, this.value);
         };
 
         // Private
-        function _insert(newNode, node) {
-            if (node === null) {
-                return newNode;
-            } else if (newNode.getValue() < node.getValue()) {
-                node.leftChild = _insert(newNode, node.leftChild);
+        function _insert(node, treeRoot) {
+            if (treeRoot === null) {
                 return node;
-            } else if (newNode.getValue() > node.getValue()) {
-                node.rightChild = _insert(newNode, node.rightChild);
-                return node;
+            } else if (node.getKey() < treeRoot.getKey()) {
+                treeRoot.leftChild = _insert(node, treeRoot.leftChild);
+                return treeRoot;
+            } else if (node.getKey() > treeRoot.getKey()) {
+                treeRoot.rightChild = _insert(node, treeRoot.rightChild);
+                return treeRoot;
             } else {
-                return node;
+                return treeRoot;
             }
         }
 
-        function _getMin(node) {
-            if (node.leftChild === null) {
-                return node;
+        function _getMin(treeRoot) {
+            if (treeRoot.leftChild === null) {
+                return treeRoot;
             }
-            return _getMin(node.leftChild);
+            return _getMin(treeRoot.leftChild);
         }
 
-        function _getMax(node) {
-            if (node.rightChild === null) {
-                return node;
+        function _getMax(treeRoot) {
+            if (treeRoot.rightChild === null) {
+                return treeRoot;
             }
-            return _getMax(node.rightChild);
+            return _getMax(treeRoot.rightChild);
         }
 
-        function _contains(searchNode, node) {
-            if (node === null) {
+        function _contains(node, treeRoot) {
+            if (treeRoot === null) {
                 return false;
-            } else if (getValue(searchNode) < node.getValue()) {
-                return _contains(searchNode, node.leftChild);
-            } else if (getValue(searchNode) > node.getValue()) {
-                return _contains(searchNode, node.rightChild);
+            } else if (getKey(node) < treeRoot.getKey()) {
+                return _contains(node, treeRoot.leftChild);
+            } else if (getKey(node) > treeRoot.getKey()) {
+                return _contains(node, treeRoot.rightChild);
             } else {
                 return true;
             }
         }
 
-        function _traverse(node, operation) {
-            if (node) {
-                if (node.leftChild !== null) {
-                    _traverse(node.leftChild, operation);
+        function _traverse(treeRoot, operation) {
+            if (treeRoot) {
+                if (treeRoot.leftChild !== null) {
+                    _traverse(treeRoot.leftChild, operation);
                 }
 
-                operation.call(this, node.content);
+                operation.call(this, treeRoot.value);
 
-                if (node.rightChild !== null) {
-                    _traverse(node.rightChild, operation);
+                if (treeRoot.rightChild !== null) {
+                    _traverse(treeRoot.rightChild, operation);
                 }
             }
         }
 
-        function _remove(x, node, parent) {
-            if (node === null) {
+        function _remove(node, treeRoot, parent) {
+            if (treeRoot === null) {
                 return;
             }
 
-            if (getValue(x) < node.getValue()) {
-                _remove(x, node.leftChild, node);
-            } else if (getValue(x) > node.getValue()) {
-                _remove(x, node.rightChild, node);
-            } else if (node.leftChild !== null && node.rightChild !== null) {
-                node.content = _getMin(node.rightChild).content;
-                _remove(node.content, node.rightChild, node);
+            if (getKey(node) < treeRoot.getKey()) {
+                _remove(node, treeRoot.leftChild, treeRoot);
+            } else if (getKey(node) > treeRoot.getKey()) {
+                _remove(node, treeRoot.rightChild, treeRoot);
+            } else if (treeRoot.leftChild !== null && treeRoot.rightChild !== null) {
+                treeRoot.value = _getMin(treeRoot.rightChild).value;
+                _remove(treeRoot.value, treeRoot.rightChild, treeRoot);
             } else {
                 if (parent) {
-                    if (node.getValue() < parent.getValue()) {
-                        parent.leftChild = node.leftChild || node.rightChild;
+                    if (treeRoot.getKey() < parent.getKey()) {
+                        parent.leftChild = treeRoot.leftChild || treeRoot.rightChild;
                     } else {
-                        parent.rightChild = node.leftChild || node.rightChild;
+                        parent.rightChild = treeRoot.leftChild || treeRoot.rightChild;
                     }
                 } else {
-                    _root = node.leftChild || node.rightChild;
+                    _root = treeRoot.leftChild || treeRoot.rightChild;
                 }
             }
 
         }
 
         // Public
-        this.contains = function(searchNode) {
-            return _contains(searchNode, _root);
+        this.contains = function(node) {
+            return _contains(node, _root);
         };
 
-        this.insert = function(content) {
+        this.insert = function(node) {
             if (!_root) {
-                _root = new Node(content, null, null);
+                _root = new Node(node, null, null);
             } else {
-                _root = _insert(new Node(content, null, null), _root);
+                _root = _insert(new Node(node, null, null), _root);
             }
         };
 
-        this.remove = function(removeNode) {
-            _remove(removeNode, _root, null);
+        this.remove = function(node) {
+            _remove(node, _root, null);
         };
 
         this.traverse = function(operation) {
@@ -128,19 +128,19 @@
         };
 
         this.toArray = function() {
-            var array = [];
+            var arr = [];
             _traverse(_root, function(node) {
-                array.push(node);
+                arr.push(node);
             });
-            return array;
+            return arr;
         };
 
-        this.arrayToTree = function(array) {
-            if (array.length !== 0) {
-                var middle = Math.floor(array.length / 2);
-                _insert(array[middle], _root);
-                this.arrayToTree(array.slice(0, middle));
-                this.arrayToTree(array.slice(middle + 1));
+        this.arrayToTree = function(arr) {
+            if (arr.length !== 0) {
+                var middle = Math.floor(arr.length / 2);
+                _insert(arr[middle], _root);
+                this.arrayToTree(arr.slice(0, middle));
+                this.arrayToTree(arr.slice(middle + 1));
             } else {
                 return;
             }
@@ -157,19 +157,15 @@
         };
 
         this.getMin = function() {
-            return _getMin(_root).content;
+            return _getMin(_root).value;
         };
 
         this.getMax = function() {
-            return _getMax(_root).content;
+            return _getMax(_root).value;
         };
 
         this.getRoot = function() {
-            return _root.content;
-        };
-
-        this.getRootValue = function() {
-            return _root.getValue();
+            return _root.value;
         };
 
         this.getTree = function() {
@@ -182,8 +178,8 @@
 
     }
 
-    exports.create = function(value) {
-        return new BinarySearchTree(value);
+    exports.create = function(customKeyFn) {
+        return new BinarySearchTree(customKeyFn);
     };
 
 
